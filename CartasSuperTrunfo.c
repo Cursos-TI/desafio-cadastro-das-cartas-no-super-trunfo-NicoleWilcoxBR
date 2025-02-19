4ffc6b076837a5062abf7679e7ef769a77a4416e
Nicole Wilcox. <nicolewilcox@outlook.com.br>
2025-02-19 17:33:14 +0000

    atualizando script

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,57 @@
+/*Biblioteca padrao de entrada e saida*/
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste Eric Douglas Koga
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+/*Criei uma funcao para carta A com todas as variaveis e exibicao na tela*/
+void carta1(){
+    char estado = 'A';
+    char codigo[] = "A01";
+    char nomeCidade[] = "Sao Paulo";
+    double populacao = 12325000;
+    float area = 1521.11;
+    float pib = 699.28;
+    int pontosTuristicos = 50;
 
+    printf("Carta A\n");
+    printf("Estado: %c \n", estado);
+    printf("Codigo: %s \n", codigo);
+    printf("Nome da cidade: %s \n", nomeCidade);
+    printf("Numero de habitantes: %f \n", populacao);
+    printf("Area em Km: %.2f km² \n", area);
+    printf("Numero de pontos turisticos: %d \n", pontosTuristicos);
+}
+
+/*Criei uma funcao para carta B com todas as variaveis e exibicao na tela*/
+void carta2(){
+    char estado = 'B';
+    char codigo[] = "B02";
+    char nomeCidade[] = "Rio de Janeiro";
+    double populacao = 6748000;
+    float area = 1200.25;
+    float pib = 300.50;
+    int pontosTuristicos = 30;
+
+    printf("Carta B\n");
+    printf("Estado: %c \n", estado);
+    printf("Codigo: %s \n", codigo);
+    printf("Nome da cidade: %s \n", nomeCidade);
+    printf("Numero de habitantes: %f \n", populacao);
+    printf("Area em Km: %.2f km² \n", area);
+    printf("Numero de pontos turisticos: %d \n", pontosTuristicos);
+}
+
+int main(){
+    /* Exibindo as informações chamando as funções na main principal*/
+    printf("Super trunfo das cidades \n");
+    printf("\t===========================\n");
+    carta1();
+    printf("\t===========================\n");
+    carta2();
+    printf("\t===========================\n");
     return 0;
 }
